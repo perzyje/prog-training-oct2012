461bc1dcfa30bab6573a506bc25a715a461f177e
Attila Balazs <attila.balazs@tora.com>
2012-10-26 14:57:01 +0300

    Resolved sonar warning

--- day00/src/main/java/com/tora/SpaceOddity.java
+++ day00/src/main/java/com/tora/SpaceOddity.java
@@ -2,6 +2,10 @@ package com.tora;
 
 
 public final class SpaceOddity {
+	private SpaceOddity() {
+		throw new AssertionError("Should not be called!");
+	}
+	
 	public static boolean isOdd(long value) {
 		return Math.abs(value) % 2 == 1;
 	}
