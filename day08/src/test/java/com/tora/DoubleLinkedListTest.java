90267ae3a60c04f97f019db1245f97e335ab30ca
Istvan-Zsolt Illyes <isti_illyes@yahoo.com>
2012-11-12 12:21:31 +0200

    Implementat iterator

--- day08/src/test/java/com/tora/DoubleLinkedListTest.java
+++ day08/src/test/java/com/tora/DoubleLinkedListTest.java
@@ -2,50 +2,70 @@ package com.tora;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
-public class DoubleLinkedListTest<E> {
-	private final DoubleLinkedList<E> list = new DoubleLinkedList<E>();
+public class DoubleLinkedListTest {
+	private List<String> list;
+
 	@Test
 	public void isEmptyTest() {
-		assertEquals(0,list.size());
+		list = new DoubleLinkedList<String>();
+		assertEquals(0, list.size());
 	}
-	
-	@Test 
-	public void addToIndexTest(){
-		list.add(0,(E) "element");
-		assertEquals(1,list.size());
-		list.add(0,(E) "element");
-		assertEquals(2,list.size());
-	}
-	
+
 	@Test
-	public void addElementTest(){
-		list.add((E) "element");
-		assertEquals(1,list.size());
+	public void addToIndexTest() {
+		list = new DoubleLinkedList<String>();
+		list.add(0, "element");
+		assertEquals(1, list.size());
+		list.add(0, "element");
+		assertEquals(2, list.size());
 	}
-	
-	@Test(expected=IndexOutOfBoundsException.class)
-	public void addToNonExistingElement(){
-		list.add(-1,(E) "test");
-		list.add(list.size()+1,(E) "test");
+
+	@Test
+	public void addElementTest() {
+		list = new DoubleLinkedList<String>();
+		list.add("element");
+		assertEquals(1, list.size());
 	}
-	
-	@Test(expected=IndexOutOfBoundsException.class)
-	public void accessToNonExistingElement(){
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void addToNonExistingElement() {
+		list = new DoubleLinkedList<String>();
+		list.add(-1, "test");
+		list.add(list.size() + 1, "test");
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void accessToNonExistingElement() {
+		list = new DoubleLinkedList<String>();
 		list.get(-1);
-		list.get(list.size()+1);
+		list.get(list.size() + 1);
 	}
-	
+
 	@Test
-	public void addAllTest(){
-		list.add((E) "Test");
-		list.add((E) "Test");
-		list.add((E) "Test");
-		list.add((E) "Test");
-		DoubleLinkedList<E> listTest = new DoubleLinkedList<E>();
+	public void addAllTest() {
+		list = new DoubleLinkedList<String>();
+		list.add("Test");
+		list.add("Test");
+		list.add("Test");
+		list.add("Test");
+		DoubleLinkedList<String> listTest = new DoubleLinkedList<String>();
 		listTest.addAll(0, list);
-		assertEquals(4,listTest.size());
+		assertEquals(4, listTest.size());
+	}
+
+	@Test
+	public void removeElementTest() {
+		list = new DoubleLinkedList<String>();
+		list.add("test1");
+		list.add("test2");
+		assertTrue(list.remove("test1"));
+		assertEquals(1, list.size());
+		assertTrue(list.remove("test2"));
+		assertEquals(0, list.size());
 	}
 
 }
