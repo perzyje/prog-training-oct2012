d33a841c5207224b74de681fedf503f250defb2c
Attila-Mihaly Balazs <attila@knowlabs.com>
2013-01-18 15:02:36 +0200

    Added tests and skeleton for BiDiMultiMap

---
+++ day25/src/main/java/com/tora/BiDiMultiMap.java
@@ -0,0 +1,32 @@
+package com.tora;
+
+import java.util.*;
+
+public final class BiDiMultiMap<K, V> extends AbstractMap<K, V> implements Map<K, V> {
+	public Collection<V> getValues(K key) {
+		// TODO
+		return null;
+	}
+	
+	public Collection<K> getKeys(V value) {
+		// TODO
+		return null;
+	}
+	
+	public Collection<V> removeAllValues(K key) {
+		// TODO
+		return null;
+	}
+	
+	public Collection<V> removeAllKeys(V value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
